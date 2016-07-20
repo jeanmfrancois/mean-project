bd83e05b154a5cc5c62ca498bee1d8b3475b9664
Jean-francois Nepton <sqasolution@hotmail.com>
2016-07-20 00:53:01 -0700

    Create Mongo Database

---
+++ app_server/models/db.js
@@ -0,0 +1,49 @@
+var mongoose = require("mongoose");
+var readLine = require ("readline");
+
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+	console.log('DB Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+	console.log('DB Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+	console.log('DB Mongoose disconnected');
+});
+
+var gracefulShutdown = function (msg, callback) {
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+
+process.once('SIGUSR2', function () {
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+process.on('SIGINT', function () {
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+	});
+});
+process.on('SIGTERM', function() {
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
+});
+
+// Windows
+if (process.platform === "win32"){
+var rl = readLine.createInterface ({
+input: process.stdin,
+output: process.stdout
+});
+rl.on ("SIGINT", function (){
+process.emit ("SIGINT");
+});
+}
