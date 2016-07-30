bccd4f497956bf6610d3c866c10cc1efcd62bd43
jeanmfrancois1 <jeanmfrancois@gmail.com>
2016-07-30 17:28:51 +0000

    Add Mongod and Mongoose Link in API Level

---
+++ app_api/models/db.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose");
+var readLine = require ("readline");
+var locations = require("./locations");
+
+var dbURI = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+	dbURI = process.env.MONGOLAB_URI;
+}
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+	console.log('DB Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+	console.log('DB Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+	console.log('DB Mongoose disconnected');
+});
+
+var gracefulShutdown = function (msg, callback) {
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+
+process.once('SIGUSR2', function () {
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+process.on('SIGINT', function () {
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+	});
+});
+process.on('SIGTERM', function() {
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
+});
+
+// Windows
+if (process.platform === "win32"){
+	var rl = readLine.createInterface ({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on ("SIGINT", function (){
+	process.emit ("SIGINT");
+});
+}
